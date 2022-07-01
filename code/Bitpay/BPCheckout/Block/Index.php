835b9dd3959ba65000cf4be974f82c792dc9e5de
Matt Sherer <matt.s@swiftotter.com>
2022-07-01 10:34:08 -0600

    TPM-59 - Updating structure to make this a composer installable package and updating the php version requirements

--- code/Bitpay/BPCheckout/Block/Index.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Bitpay\BPCheckout\Block;
-class Index extends \Magento\Framework\View\Element\Template
-{
-
-}
--- code/Bitpay/BPCheckout/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Bitpay_BPCheckout',
-    __DIR__
-);
